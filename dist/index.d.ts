6a1466dbcea019a86a152d87079b6d091f50f761
D Hardy <dhardy@HQPRO.local>
2019-10-22 15:33:33 -0400

    added export of client

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './common';
 export * from './providers';
+export * from './client';
 export * from './jsforce.module';
--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,5 @@ function __export(m) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __export(require("./common"));
 __export(require("./providers"));
+__export(require("./client"));
 __export(require("./jsforce.module"));
