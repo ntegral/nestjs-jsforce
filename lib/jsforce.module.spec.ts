ffabfac44c6ee51cb852a8f7afd39797f63790bf
D Hardy <dhardy@HQPRO.local>
2019-10-22 12:33:16 -0400

    updated the tests to add more coverage.

--- lib/jsforce.module.spec.ts
+++ lib/jsforce.module.spec.ts
@@ -1,17 +1,80 @@
+import { Test } from '@nestjs/testing';
 import { JsForceModule } from "./jsforce.module";
-import { JsForceConfigOptions } from "./common/interfaces";
-import { JSFORCE_MODULE_OPTIONS } from "./common";
+import { JsForceConfigOptions, JsForceConfigOptionsFactory } from "./common/interfaces";
+import { JSFORCE_MODULE_OPTIONS, JSFORCE_TOKEN } from "./common";
 import { config } from "rxjs";
+import { async } from "rxjs/internal/scheduler/async";
+import { Client } from './client';
+import { expression } from '@babel/template';
 
 describe('JsForceModule', () => {
+    let config: JsForceConfigOptions = {
+        username: 'dexter.hardy@testaccount.com.sb01',
+        password: 'somepassword',
+        options: {
+            loginUrl: 'https://someaccount--sb01.my.salesforce.com/'
+        }
+    };
+
+    class TestService implements JsForceConfigOptionsFactory {
+        createJsForceOptions(): JsForceConfigOptions {
+            return config;
+        }
+    }
+
+    describe('forRoot 1.0', () => {
+        it('should provide the jsforce client', async() => {
+            const mod = await Test.createTestingModule({
+                imports: [JsForceModule.forRoot(config)],
+            }).compile();
+
+            const jsforceClient = mod.get<Client>(JSFORCE_TOKEN);
+            expect(jsforceClient).toBeDefined();
+            expect(jsforceClient).toBeInstanceOf(Client);
+        })
+    });
+
+    describe('forRootAsync 1.0', () => {
+        describe('when the `useFactory` option is used', () => {
+            it('should provide the jsforce client', async () => {
+                const mod = await Test.createTestingModule({
+                    imports: [
+                        JsForceModule.forRootAsync({
+                            useFactory: () => (config),
+                        }),
+                    ]
+                }).compile();
+
+                const jsforceClient = mod.get<Client>(JSFORCE_TOKEN);
+                expect(jsforceClient).toBeDefined();
+                expect(jsforceClient).toBeInstanceOf(Client);
+            });
+        })
+    });
+
+    describe('when the `useClass` option is used', () => {
+        it('should provide the jsforce client', async () => {
+            const mod = await Test.createTestingModule({
+                imports: [
+                    JsForceModule.forRootAsync({
+                        useClass: TestService
+                    })
+                ]
+            }).compile();
+
+            const jsforceClient = mod.get<Client>(JSFORCE_TOKEN);
+            expect(jsforceClient).toBeDefined();
+            expect(jsforceClient).toBeInstanceOf(Client);
+        });
+    })
     describe('forRoot', () => {
-        let config: JsForceConfigOptions = {
+        /* let config: JsForceConfigOptions = {
             username: 'dexter.hardy@testaccount.com.sb01',
             password: 'somepassword',
             options: {
                 loginUrl: 'https://someaccount--sb01.my.salesforce.com/'
             }
-        };
+        }; */
         it('should be DynamicModule', () => {
             const dynamicModule = JsForceModule.forRoot(config);
 
@@ -23,19 +86,7 @@ describe('JsForceModule', () => {
                 useFactory: () => config
             });
 
-            expect(dynamicModule).toBeDefined();
-
-            expect(dynamicModule).toMatchObject({
-                imports: [],
-                module: JsForceModule,
-                providers: [
-                    {
-                        inject: [],
-                        provide: 'JSFORCE_MODULE_OPTIONS',
-                    },
-                ],
-            });
-            
+            expect(dynamicModule).toBeDefined();            
         })
     });
 })
